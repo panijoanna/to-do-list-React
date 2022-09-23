9251d7c09705152d8a9231b169613c70c2f3b735
Joanna Panimasz <pani.joanna93@gmail.com>
2022-09-23 15:56:23 +0200

    changed atributes names

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Form = () => (
     <form className="form">
                 <input className="form__field" required type="text" name="lista"
-                    placeholder="Co jest do zrobienia?" maxlength="70" autocomplete="off"/>
+                    placeholder="Co jest do zrobienia?" maxLength="70" autoComplete="off"/>
                 <button className="form__button">Dodaj zadanie</button>
             </form>
 );
