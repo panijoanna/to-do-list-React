bb882243253cfcfbef4e5aceb9554b626aa0b758
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 12:40:08 +0100

    add Form styles to styled.js

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -11,3 +11,32 @@ export const FormElement = styled.form`
     flex-direction: column;
   }
 `;
+
+export const Input = styled.input`
+  padding: 8px;
+  width: 100%;
+  display: flex;
+  align-self: center;
+  border: 2px solid #e2e2e2;
+`;
+
+export const FormButton = styled.button`
+  background-color: #027381;
+  color: #fff;
+  padding: 10px;
+  margin-left: 20px;
+  border: none;
+  width: 200px;
+  cursor: pointer;
+  transition: 0.5s;
+
+  &:hover {
+    background: hsl(187, 100%, 31%);
+    transform: scale(1.1);
+  }
+
+  @media (max-width: 767px) {
+    width: 100%;
+    margin: 10px 0;
+  }
+`;
