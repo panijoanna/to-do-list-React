82125851768e4d2b71336362ee4cadcb745d08d2
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 12:59:03 +0100

    add SectionHeading component to Section

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,7 +3,9 @@ import { StyledSection } from "./styled";
 const Section = ({ title, body, extraHeaderContent }) => (
   <StyledSection>
     <header className="section__header section__header--toggleButtons">
-      <h3 className="section__heading">{title}</h3>
+      <SectionHeading>
+          {title}
+      </SectionHeading>
       {extraHeaderContent}
     </header>
     <div className="section__container">{body}</div>
