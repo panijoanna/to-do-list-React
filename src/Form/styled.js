cc4f27fda789cacf053bf142081b21d0f55a04ce
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 12:34:25 +0100

    add media query to form in styled.js

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -4,4 +4,10 @@ export const FormElement = styled.form`
   padding: 10px 0;
   justify-content: space-between;
   display: flex;
+
+  @media (max-width: 767px) {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
 `;
