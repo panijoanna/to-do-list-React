d0563ac8355640d341fbf057e82d8df2d454801f
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 11:04:10 +0100

    import Button component to Tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,24 +1,25 @@
-import { TasksList, Item, Content } from "./styled";
+import { TasksList, Item, Content, Button } from "./styled";
 
 const Tasks = ({ tasks, hideDoneTasks, removeTask, toggleTaskDone }) => (
   <TasksList>
     {tasks.map((task) => (
-      <Item key={task.id} hidden={task.done && hideDoneTasks}>
-        <button
-          className="tasks__button tasks__button--toggleDone"
-          onClick={() => toggleTaskDone(task.id)}
-        >
+      <Item 
+        key={task.id} 
+        hidden={task.done && hideDoneTasks}>
+        <Button 
+          toggleDone 
+          onClick={() => toggleTaskDone(task.id)}>
           {task.done ? "✓" : ""}
-        </button>
-        <Content done={task.done}>
+        </Button>
+        <Content 
+          done={task.done}>
           {task.content}
-        </Content>
-        <button
-          className="tasks__button tasks__button--remove"
-          onClick={() => removeTask(task.id)}
-        >
+          </Content>
+        <Button 
+          remove 
+          onClick={() => removeTask(task.id)}>
           🗑
-        </button>
+        </Button>
       </Item>
     ))}
   </TasksList>
