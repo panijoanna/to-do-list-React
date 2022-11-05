9b39ac9d49fad569220a45d8b24d9596a310fe44
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 12:52:31 +0100

    export and add StyledSection component

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -1,2 +1,12 @@
 import styled from "styled-components";
 
+export const StyledSection = styled.section`
+  background-color: white;
+  box-shadow: 0 2px #eee;
+  display: flex;
+  flex-direction: column;
+  max-width: 900px;
+  margin: 10px 0;
+  border: 1px solid #e2e2e2;
+  padding: 5px 20px;
+`;
