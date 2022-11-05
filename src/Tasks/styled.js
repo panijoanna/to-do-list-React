81b71a0e68539e44dc9fc423f4f970428e670cd4
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 10:57:01 +0100

    add button styles to styled.js

--- src/Tasks/styled.js
+++ src/Tasks/styled.js
@@ -38,13 +38,31 @@ export const Button = styled.button`
   padding: 0;
   transition: 0.5s;
 
-  ${({toggleDone}) =>
+  ${({ toggleDone }) =>
     toggleDone &&
     css`
       background: hsl(120, 61%, 39%);
 
       &:hover {
         background: hsl(123, 64%, 33%);
+      }
+
+      &:active {
+        background: hsl(120, 61%, 44%);
+      }
+    `}
+    
+  ${({ remove }) =>
+    remove &&
+    css`
+      background: hsl(348, 83%, 47%);
+
+      &:hover {
+        background: hsl(355, 98%, 60%);
+      }
+
+      &:active {
+        background: hsl(348, 83%, 57%);
       }
     `}
 `;
