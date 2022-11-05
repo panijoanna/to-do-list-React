822edacab8e7b045d5e65ba809345e86e6d08d77
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 12:58:09 +0100

    add StyledSection component to Section

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,13 +1,13 @@
-import "./style.css";
+import { StyledSection } from "./styled";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-  <section className="section">
+  <StyledSection>
     <header className="section__header section__header--toggleButtons">
       <h3 className="section__heading">{title}</h3>
       {extraHeaderContent}
     </header>
     <div className="section__container">{body}</div>
-  </section>
+  </StyledSection>
 );
 
 export default Section;
