12ce0f0f3cbf7d4c26527642c3acec795f61e6e4
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 12:18:16 +0100

    export and add ListsButton component in styled.js

--- src/Buttons/styled.js
+++ src/Buttons/styled.js
@@ -1,1 +1,20 @@
 import styled from "styled-components";
+
+export const ListsButton = styled.button `
+  color: hsl(187, 97%, 26%);
+  border: none;
+  background-color: transparent;
+  transition: 0.5s;
+
+  &:hover {
+    color: hsl(185, 36%, 50%);
+  }
+
+  &:disabled {
+    color: hsl(0, 0%, 69%);
+  }
+
+  @media (max-width: 767px) {
+      padding: 10px;
+    }
+`;
