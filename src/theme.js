95f10a1340bdff57fac2e5ac5ec7385a16b24212
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-07 10:39:36 +0100

    add theme with colors

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+    color: {
+        lagoon: "#017382",
+        blue: "#51A5AD",
+        silver: "#AFAFAF",
+        mercury: "#e2e2e2",
+        skobeloff: "#027381",
+        white: "#fff",
+        cerulean: "#008B9E",
+        galery: "#eee",
+        alto: "#ddd",
+        forest: "#26A026",
+        green: "#1E8A23",
+        chartreuse: "#2BB42B",
+        crimson: "#DB143C",
+        coral: "#FC3545",
+        amaranth: "#EC365A";
+    }
+};
