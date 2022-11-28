17e0f6cda43491a5ce537d8b36f17a6f25261644
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-28 18:34:48 +0100

    add Tasks file

---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Form from "./Form";
+import TaskList from "./TaskList";
+import Buttons from "./Buttons";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import { useTasks } from "../../useTasks";
+
+function Tasks() {
+  const [hideDoneTasks, setHideDoneTasks] = useState(false);
+
+  const hideTasks = () => {
+    setHideDoneTasks((hideDoneTasks) => !hideDoneTasks);
+  };
+
+  const { tasks, removeTask, toggleAllTaskDone, toggleTaskDone, addTask } =
+    useTasks();
+
+  return (
+    <>
+      <Header title="Lista zadań" />
+      <Section 
+        title="Dodaj nowe zadanie" 
+        body={<Form addTask={addTask} />} 
+      />
+
+      <Section
+        title="Lista zadań"
+        body={
+          <TaskList
+            tasks={tasks}
+            hideDoneTasks={hideDoneTasks}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDoneTasks={hideDoneTasks}
+            hideTasks={hideTasks}
+            toggleAllTaskDone={toggleAllTaskDone}
+          />
+        }
+      />
+    </>
+  );
+}
+
+export default Tasks;
