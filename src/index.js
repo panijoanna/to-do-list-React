448e6dfa699e2f1c0fc22268b1bb3bce18b4228a
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-30 12:49:22 +0100

    add Provider to the root

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from './GlobalStyle';
-import { theme } from './theme';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GlobalStyle } from "./GlobalStyle";
+import { theme } from "./theme";
+import { ThemeProvider } from "styled-components";
 import store from "./store.js";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <ThemeProvider theme={theme}>
-        <GlobalStyle/>
-        <App/>
-        </ThemeProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function to
