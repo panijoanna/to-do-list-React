6c8f29f2aaabfa678f3b5d61c5774e1fec670802
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-20 16:21:27 +0100

    add consts to the Buttons with selectors

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,10 +1,16 @@
 import { ListsButton, SectionButton } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, hideTasks } from "../tasksSlice.js";
+import { selectTasks, hideTasks, selectEmptyTasks, selectEveryDoneTasks, selectHideDoneTasks } from "../tasksSlice.js";
 
 const Buttons = ({ toggleAllTaskDone }) => {
+
+const hideDoneItemTasks = useSelector(selectHideDoneTasks);
+const areTasksEmpty = useSelector(selectEmptyTasks);
+const areTasksDone = useSelector(selectEveryDoneTasks);
+
   const { tasks, hideDoneTasks } = useSelector(selectTasks);
   const dispatch = useDispatch();
+
   return (
     tasks.length > 0 && (
       <SectionButton>
