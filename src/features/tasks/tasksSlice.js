654f0b5a6a8f1b53725c1b52c7567cde336748be
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-20 14:45:30 +0100

    do a destructuring in toggleTaskDone

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -1,25 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const tasksSlice = createSlice({
-    name: "tasks",
-    initialState: {
-        tasks: [],
-        hideDoneTasks: false,
+  name: "tasks",
+  initialState: {
+    tasks: [],
+    hideDoneTasks: false,
+  },
+  reducers: {
+    addTask: ({ tasks }, { payload }) => {
+      tasks.push(payload);
     },
-    reducers: {
-        addTask: ({ tasks }, { payload }) => {
-            tasks.push(payload);
-        },
-        hideTasks: state => {
-            state.hideDoneTasks = !state.hideDoneTasks;
-        },
-        toggleTaskDone: (state, action) => {
-            const index = state.tasks.findIndex(task => task.id === action.payload);
-            state.tasks[index].done = !state.tasks[index].done;
-        },
+    hideTasks: (state) => {
+      state.hideDoneTasks = !state.hideDoneTasks;
     },
+    toggleTaskDone: ({ tasks }, { payload }) => {
+      const index = tasks.findIndex(({ id }) => id === payload);
+      tasks[index].done = !tasks[index].done;
+    },
+  },
 });
 
 export const { addTask, hideTasks, toggleTaskDone } = tasksSlice.actions;
-export const selectTasks = state => state.tasks;
+export const selectTasks = (state) => state.tasks;
 export default tasksSlice.reducer;
