60523d2b9cef189746f199c276f9aa3c61897ab7
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-20 16:03:29 +0100

    import toggleAllTaskDone, removeTask in tasksSlice

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -29,6 +29,6 @@ const tasksSlice = createSlice({
   },
 });
 
-export const { addTask, hideTasks, toggleTaskDone } = tasksSlice.actions;
+export const { addTask, hideTasks, toggleTaskDone, toggleAllTaskDone, removeTask } = tasksSlice.actions;
 export const selectTasks = (state) => state.tasks;
 export default tasksSlice.reducer;
