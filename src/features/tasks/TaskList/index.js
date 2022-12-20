033a856258c14ec983d8be05605a775a44486383
Joanna Panimasz <pani.joanna93@gmail.com>
2022-12-20 12:05:26 +0100

    add return to the TaskList

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,29 +1,22 @@
 import { List, Item, Content, Button } from "./styled";
 import { useSelector } from "react-redux";
 
-const TaskList = ({ removeTask, toggleTaskDone }) => (
-  <List>
-    {tasks.map((task) => (
-      <Item 
-        key={task.id} 
-        hidden={task.done && hideDoneTasks}>
-        <Button 
-          toggleDone 
-          onClick={() => toggleTaskDone(task.id)}>
-          {task.done ? "✓" : ""}
-        </Button>
-        <Content 
-          done={task.done}>
-          {task.content}
-          </Content>
-        <Button 
-          remove 
-          onClick={() => removeTask(task.id)}>
-          🗑
-        </Button>
-      </Item>
-    ))}
-  </List>
-);
+const TaskList = ({ removeTask, toggleTaskDone }) => {
+  return (
+    <List>
+      {tasks.map((task) => (
+        <Item key={task.id} hidden={task.done && hideDoneTasks}>
+          <Button toggleDone onClick={() => toggleTaskDone(task.id)}>
+            {task.done ? "✓" : ""}
+          </Button>
+          <Content done={task.done}>{task.content}</Content>
+          <Button remove onClick={() => removeTask(task.id)}>
+            🗑
+          </Button>
+        </Item>
+      ))}
+    </List>
+  );
+};
 
 export default TaskList;
