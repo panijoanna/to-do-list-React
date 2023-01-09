bf87500aba702c91782442e6a3b605e28bdb0c98
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-09 21:11:24 +0100

    import fetchExampleTasks and setTasks in tasksSaga

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,2 +1,4 @@
 import { takeEvery, call, put } from "redux-saga/effects";
 import { getExampleTasks } from "./getExampleTasks";
+import { fetchExampleTasks } from "./tasksSlice";
+import { setTasks } from "./tasksSlice";
