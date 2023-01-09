1aea9f61e61b79e98f6803a1be739a8e371af26d
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-09 21:16:58 +0100

    add async await and error to the getExampleTasks

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,3 +1,8 @@
 export const getExampleTasks = async () => {
-    
-}
+  const response = await fetch("/to-do-list-React/exampleTasks.json");
+
+  if (!response.ok) {
+    new Error(response.statusText);
+  }
+  return await response.json();
+};
