33cee786d356c96fea84161a0cd7a96f1e330ebe
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-09 21:20:33 +0100

    import createSagaMiddleware, rootSaga in store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./features/tasks/tasksSlice";
+import createSagaMiddleware from "@redux-saga/core";
+import rootSaga from "./rootSaga";
 
 export default configureStore({
   reducer: {
