a27954af5d89d9429b9c7ecab2d4fd78f3907dd7
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-11 20:20:41 +0100

    insert styled components to NavBar

--- src/features/NavBar/index.js
+++ src/features/NavBar/index.js
@@ -1,7 +1,16 @@
-
+import { NavigationLink, NavLists, NavUl, StyledNavigation } from "./styled";
 
 const NavBar = () => {
-
+  return (
+    <StyledNavigation>
+      <NavUl>
+        <NavLists>Zadania</NavLists>
+        <NavLists>
+          <NavigationLink to="/o autorze">O autorze</NavigationLink>
+        </NavLists>
+      </NavUl>
+    </StyledNavigation>
+  );
 };
 
 export default NavBar;
