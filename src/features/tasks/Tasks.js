b9a5b8961f98c4558aea4f72c7eceaf445d94b77
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-11 20:28:59 +0100

    add NavBar component to the Tasks

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -3,10 +3,12 @@ import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
+import NavBar from "../NavBar";
 
 function Tasks() {
   return (
     <>
+      <NavBar />
       <Header title="Lista zadań" />
       <Section title="Dodaj nowe zadanie" body={<Form />} />
       <Section
