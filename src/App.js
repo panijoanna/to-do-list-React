57072cc5175d9106cd0d63da56df702f9c751fec
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-11 16:12:13 +0100

    instert Routes in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Author from "./features/Author";
 
 const App = () => (
- 
+  <>
+    <Routes>
+      <Route path="/to-do-list-React" element={<Tasks />}></Route>
+      <Route path="/o autorze" element={<Author />}></Route>
+    </Routes>
+  </>
 );
 
 export default App;
