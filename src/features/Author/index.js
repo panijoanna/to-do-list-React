09152a9a930d638a0b73fe6ec4f636a92f81411d
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-11 20:26:21 +0100

    insert components to the index.js in Author

--- src/features/Author/index.js
+++ src/features/Author/index.js
@@ -1,7 +1,22 @@
-
+import { NavigationLink, NavLists, StyledNavigation } from "../NavBar/styled";
 
 const Author = () => {
-
+  return (
+    <>
+      <StyledNavigation>
+        <NavUl>
+          <NavLists>
+            <NavigationLink to="/to-do-list-React">Zadania</NavigationLink>
+          </NavLists>
+          <NavLists>
+            <NavigationLink to="/o autorze">O autorze</NavigationLink>
+          </NavLists>
+        </NavUl>
+      </StyledNavigation>
+      <Header title="O autorze"></Header>
+      <Section title="Joanna Panimasz" body={<>Hello</>}></Section>
+    </>
+  );
 };
 
 export default Author;
