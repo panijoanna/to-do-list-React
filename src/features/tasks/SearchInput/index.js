d65e04be6fd1202e47052a86a3df495c083482c6
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-12 18:14:17 +0100

    insert StyledInput to the SearchInput

--- src/features/tasks/SearchInput/index.js
+++ src/features/tasks/SearchInput/index.js
@@ -1,8 +1,13 @@
 import { StyledInput } from "./styled";
 
 const SearchInput = () => {
-    
+  return (
+    <StyledInput
+      required
+      placeholder="🔍Wyszukaj w liście zadań"
+      autoComplete="off"
+    />
+  );
 };
-
 
 export default SearchInput;
