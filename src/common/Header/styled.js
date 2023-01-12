0f0330c5cdaaaa64cfb21225af888b2e77867596
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-12 10:42:01 +0100

    add styles to the Heading

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
 export const Heading = styled.h2`
-  font-weight: 900;
+  display: flex;
+  flex-direction: column;
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 40px 0 20px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
+    padding: 40px 0 20px 20px;
+  }
 `;
