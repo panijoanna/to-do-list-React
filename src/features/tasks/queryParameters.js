5e441e1eca72894d0d0cb1e7d70f2618810277e4
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-16 11:20:49 +0100

    add searchParams const with URLSearchParams

--- src/features/tasks/queryParameters.js
+++ src/features/tasks/queryParameters.js
@@ -1,5 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom";
 
 export const useQueryParameter = () => {
-const location = useLocation();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  return searchParams.get(key);
 };
