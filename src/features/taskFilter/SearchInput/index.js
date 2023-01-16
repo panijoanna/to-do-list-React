76863f09e4e4accee4bba8ea8b46c9db623189e8
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-16 11:35:49 +0100

    create onInputChange const in SearchInput

--- src/features/taskFilter/SearchInput/index.js
+++ src/features/taskFilter/SearchInput/index.js
@@ -1,10 +1,20 @@
-import { useQueryParameter, useReplaceQueryParameter } from "../../tasks/queryParameters";
+import {
+  useQueryParameter,
+  useReplaceQueryParameter,
+} from "../../tasks/queryParameters";
 import searchQueryParamName from "../../tasks/searchQueryParamName";
 import { StyledInput } from "./styled";
 
 const SearchInput = () => {
   const query = useQueryParameter(searchQueryParamName);
   const replaceQueryParameter = useReplaceQueryParameter();
+
+  const onInputChange = ({ target }) => {
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
+  };
   return (
     <StyledInput
       required
