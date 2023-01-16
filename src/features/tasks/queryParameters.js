a47991ff03433b3ea5ce788d0694bb6d52e9fab5
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-16 11:40:11 +0100

    add key to the useQueryParameter

--- src/features/tasks/queryParameters.js
+++ src/features/tasks/queryParameters.js
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 
-export const useQueryParameter = () => {
+export const useQueryParameter = (key) => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   return searchParams.get(key);
