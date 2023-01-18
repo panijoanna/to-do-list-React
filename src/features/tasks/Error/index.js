cbee6688da089f90c4bbc76ecdd84578cc496b1f
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-18 13:56:53 +0100

    import the necessary files

--- src/features/tasks/Error/index.js
+++ src/features/tasks/Error/index.js
@@ -1,13 +1,16 @@
 import { useParams } from "react-router-dom";
 import Header from "../../../common/Header";
 import Section from "../../../common/Section";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
+import Navigation from "../../../Navigation";
 
 const Error = () => {
-  const {id} = useParams();
-  const task = useSelector(state => getTaskById(state, id));
+  const { id } = useParams();
+  const task = useSelector((state) => getTaskById(state, id));
   return (
     <>
-    <Navigation/>
+      <Navigation />
       <Header title="Szczegóły zadania" />
       <Section
         title={task ? task.content : "Nie ma takiego zadania"}
