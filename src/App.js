53f77b3c7afb7dad59ab7e38cbc03e8e490b1618
Joanna Panimasz <pani.joanna93@gmail.com>
2023-01-18 13:39:55 +0100

    add new route to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Tasks from "./features/tasks/Tasks";
 import { Route, Routes } from "react-router-dom";
 import Author from "./features/author";
-import { toAuthor, toTask } from "./routes";
+import { toAuthor, toTask, toDetails } from "./routes";
+import Error from "./features/tasks/Error";
 
 const App = () => (
   <>
     <Routes>
       <Route path={toTask()} element={<Tasks />}></Route>
       <Route path={toAuthor()} element={<Author />}></Route>
+      <Route path={toDetails()} element={<Error />}></Route>
     </Routes>
   </>
 );
