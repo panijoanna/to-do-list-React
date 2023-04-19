dcaeaec61424988c6457ac26903b2540dcf8c026
Joanna Panimasz <pani.joanna93@gmail.com>
2023-04-19 10:37:32 +0200

    remove react fragment from App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import Tasks from "./features/tasks/Tasks";
 import { Route, Routes } from "react-router-dom";
-import Author from "./features/author";
+import Author from "./features/Author";
 import { toAuthor, toTask, toDetails } from "./routes";
 import TaskDetails from "./features/taskDetails";
 
 const App = () => (
-  <>
     <Routes>
       <Route path={toTask()} element={<Tasks />}></Route>
       <Route path={toAuthor()} element={<Author />}></Route>
       <Route path={toDetails()} element={<TaskDetails />}></Route>
     </Routes>
-  </>
 );
 
 export default App;
