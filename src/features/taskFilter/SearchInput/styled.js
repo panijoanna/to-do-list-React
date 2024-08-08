0dbbd538d36fa44565b83f76904fd16b5f8b2e80
Joanna Panimasz <pani.joanna93@gmail.com>
2024-08-08 14:33:27 +0200

    wip: change the styles and contet of SearchInput

--- src/features/taskFilter/SearchInput/styled.js
+++ src/features/taskFilter/SearchInput/styled.js
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
 export const StyledInput = styled.input`
-  padding: 8px;
+  padding: 12px;
   display: flex;
   align-self: center;
-  border: 2px solid ${({ theme }) => theme.color.mercury};
+  border-radius: 8px;
+  border: none;
+  background: ${({ theme }) => theme.color.gray};
   width: 100%;
+  font-size: 14px;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.color.osloGray};
+  }
 `;
